780241f4eab6bb573f00dba6366d7f4153bbf2fa
Neeraj Kumar Dama <reachdaneeraj999@gmail.com>
2018-01-14 17:10:55 +0530

    edited personal data

--- web/Config.php
+++ web/Config.php
@@ -3,7 +3,7 @@
  define('DB_PASSWORD','xyz.123.abc');
  define('DB_NAME','2446863_hatao');
  define('DB_HOST','fdb17.awardspace.net');
- define('FIREBASE_API_KEY','AAAAlrQMSDM:APA91bHofENI4UQSfDTE94wfRwu1Ft66WzwKpM0Y7tyfKJYcYYUCXse3IBchJOGigJyljUURn4kpoFBTDQS_EC0CpB61Tn97j33b8DKZ95yJgl2raK33IKrSN4ZnVgA4PlmlL6LJW2Bw');
+ define('FIREBASE_API_KEY','PUT YOUR API KEY HERE');
 ?>
 
 
